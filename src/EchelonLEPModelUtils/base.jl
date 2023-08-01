381aa5e80d8430bd07fe7b7df4de0af24cfd7e1e
josePereiro <jose.pereiro.personal@gmail.com>
2023-07-31 23:48:27 -0400

    add EchelonLEPModel hash

--- src/EchelonLEPModelUtils/base.jl
+++ src/EchelonLEPModelUtils/base.jl
@@ -2,6 +2,14 @@ import Base.show
 # show(io::IO, elep::EchelonLEPModel) = (println(io, "EchelonLEPModel"); summary(io, elep))
 show(io::IO, elep::EchelonLEPModel) = println(io, "EchelonLEPModel ", size(elep))
 
-
 import Base.size
 size(elep::EchelonLEPModel) = size(elep.lep)
+
+import Base.hash
+function hash(elep::EchelonLEPModel, h::UInt64 = UInt64(0)) 
+    h += hash(:EchelonLEPModel)
+    h = hash(elep.lep, h)
+    h = hash(elep.G, h)
+    return h
+end
+hash(elep::EchelonLEPModel, h::Integer) = hash(elep, UInt64(h)) 
